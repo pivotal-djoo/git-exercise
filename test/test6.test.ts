a5dfe20fe8b261b4ca4418f27ab08520cd26e26c
David Joo <taehyunjoo@gmail.com>
2025-11-19 12:07:18 -0500

    Step 6B

--- test/test6.test.ts
+++ test/test6.test.ts
@@ -4,4 +4,8 @@ describe('Test 6', () => {
   it('adds decimal numbers', () => {
     expect(add(1.5, 2.25)).toBe(3.75);
   });
+
+  it('adds numbers resulting in zero', () => {
+    expect(add(5, -5)).toBe(0);
+  });
 });
